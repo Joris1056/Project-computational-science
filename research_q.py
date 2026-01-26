7ae076a4de50c6a685c7942a574826b5f7299ea4
Joris1056 <jorisberkenbosch@gmail.com>
2026-01-26 13:44:13 +0100

    ....

--- research_q.py
+++ research_q.py
@@ -2,15 +2,69 @@ from Main import ParkinsonSim
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.interpolate import interp1d
+from intervention_main import ParkinsonSim_intervention
 
 
 
-def plot_neuron_degradation_parkinsons(number_runs, params, title):
+def sim_parkinsons_no_intervention(number_runs, params):
+    neurons_alive_total = []
+    years_total = []
+    year_step_runs = []
+    for run in range(number_runs):
+        print(run)
+        sim = ParkinsonSim(visualize=False)
+        sim.reset()
+        for name, value in params.items():
+                setattr(sim,name,value)
+
+        done = False
+        while not done:
+            done = sim.step()
+            if sim.t >= 2000:
+                break
+        neuron_alive = [100- p for p in sim.neuron_death]
+        year_step_runs.append(sim.year_per_step)
+
+        neurons_alive_total.append(neuron_alive)
+        years_total.append(sim.time_years)
+    
+    # mean year/step for intervention simulations
+    mean_year_per_step = np.mean(year_step_runs)
+
+    t_min = 0
+    t_max = min(max(y) for y in years_total)
+    n_points = 200
+
+    common_time = np.linspace(t_min, t_max, n_points)
+
+    neurons_interp = []
+    for years, neurons in zip(years_total,neurons_alive_total):
+        f = interp1d(
+            years,
+            neurons,
+            kind='linear',
+            bounds_error=False,
+            fill_value=np.nan
+        )
+        neurons_interp.append(f(common_time))
+
+    neurons_interp = np.array(neurons_interp)
+
+    mean_neurons_alive = np.nanmean(neurons_interp, axis=0)
+    std_neurons_alive = np.nanstd(neurons_interp, axis=0)
+    CI = [mean_neurons_alive - (1.96 * (std_neurons_alive/np.sqrt(number_runs))), mean_neurons_alive + (1.96 * (std_neurons_alive/np.sqrt(number_runs)))]
+
+    index_70 = np.where(mean_neurons_alive <= 70)[0][0]
+
+    return common_time, mean_neurons_alive, CI, index_70, mean_year_per_step
+
+
+def sim_parkinsons_intervention(number_runs, params):
     neurons_alive_total = []
     years_total = []
     for run in range(number_runs):
         print(run)
-        sim = ParkinsonSim(visualize=False)
+        sim = ParkinsonSim_intervention(visualize=False)
         sim.reset()
         for name, value in params.items():
                 setattr(sim,name,value)
@@ -50,6 +104,11 @@ def plot_neuron_degradation_parkinsons(number_runs, params, title):
 
     index_70 = np.where(mean_neurons_alive <= 70)[0][0]
 
+    return common_time, mean_neurons_alive, CI, index_70
+
+
+
+def plot_neuron_degen_over_time(common_time, mean_neurons_alive, CI, index_70, title):
     plt.plot(common_time-common_time[index_70], mean_neurons_alive, label = 'mean')
     plt.fill_between(np.array(common_time)-common_time[index_70], CI[0], CI[1], alpha = 0.3, label = '95% CI')
     plt.axvline(x = common_time[index_70]-common_time[index_70], color='red', linestyle='--', label='70% neurons alive')
@@ -79,4 +138,5 @@ if __name__ == "__main__":
         'ventral_ratio_multiplication': 6,
         'dead_neighbour_multiplier': 0.03
     }
-    restults_no_intervention = plot_neuron_degradation_parkinsons(2, params_no_intervention, 'No Intervention')
+    common_time, mean_neurons_alive, CI, index_70, mean_year_per_step = sim_parkinsons_no_intervention(2, params_no_intervention)
+
